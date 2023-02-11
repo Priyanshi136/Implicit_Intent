571fb7de2661bd626d133bee08f52c8f0b146914
Priyanka Tanwar <tanwarpriyanshi02@gmail.com>
2023-02-11 13:21:02 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/implicitintent/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webBtn = findViewById<CardView>(R.id.cardWeb)
+        val cameraBtn = findViewById<CardView>(R.id.cardCamera)
+
+        webBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://priyanshi136.github.io/Personal-Portfolio/")
+            startActivity(intent)
+        }
+
+        cameraBtn.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+
+    }
+
+
+}
